1e66817d33468dfb5a6c0e95f3b0ee3d93e0a527
Patxi <pmdn@mailbox.org>
2022-10-16 13:33:54 +0200

    first commit

---
+++ src/pid_controller.h
@@ -0,0 +1,9 @@
+#ifndef PID_CONTROLLER_H
+#define PID_CONTROLLER_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+
+#endif // PID_CONTROLLER_H
---
+++ test/test_pid_controller.c
@@ -0,0 +1,22 @@
+#include "unity.h"
+#include "pid_controller.h"
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+void test_function_should_doBlahAndBlah(void) {
+    //test stuff
+}
+
+
+// tests are called here
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_function_should_doBlahAndBlah);
+    return UNITY_END();
+}
